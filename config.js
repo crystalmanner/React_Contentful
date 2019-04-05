b015bc3e884d32dc6c455a7d01e2de7970cffea9
crystalmanner <crystalmanner130@outlook.com>
2019-04-04 19:48:55 -0700

    fix bugs

---
+++ config.js
@@ -0,0 +1,4 @@
+export const deliveryAccessToken = 'fa8bac8fe7a47402e1412b831e49ddeb9b41e7a1cd32ab742dbf88804a6da697';
+export const spaceId = '3qjjkhhegtjs';
+
+export const galleryTypeId = 'photoGallery';
